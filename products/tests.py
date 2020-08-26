74bcf55fa92e193bba8a63bea4dcc47c597d064b
Trevbytes <trevor.j.adams@outlook.com>
2020-08-26 16:31:21 +0000

    add next tests

--- products/tests.py
+++ products/tests.py
@@ -8,6 +8,10 @@ class ProductModelTest(TestCase):
     def test_string_representation(self):
         product = Product(name="Product Name")
         self.assertEqual(str(product), product.name)
+        category = Category(name="Category Name")
+        self.assertEqual(str(category), category.name)
+        subcategory = SubCategory(name="Sub Name")
+        self.assertEqual(str(subcategory), subcategory.name)
 
     def test_verbose_name_plural(self):
         self.assertEqual(str(Category._meta.verbose_name_plural), "Categories")
