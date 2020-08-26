d918254d20f273bf0c81ef6cebea9601ef349453
Trevbytes <trevor.j.adams@outlook.com>
2020-08-26 17:02:17 +0000

    refactor: category models use friendly name for str

--- products/models.py
+++ products/models.py
@@ -11,7 +11,7 @@ class Category(models.Model):
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return self.friendly_name
 
 
 class SubCategory(models.Model):
@@ -23,7 +23,7 @@ class SubCategory(models.Model):
     friendly_name = models.CharField(max_length=254, null=True, blank=True)   
 
     def __str__(self):
-        return self.name
+        return self.friendly_name
 
 
 class Product(models.Model):
--- products/tests.py
+++ products/tests.py
@@ -8,10 +8,10 @@ class ProductModelTest(TestCase):
     def test_string_representation(self):
         product = Product(name="Product Name")
         self.assertEqual(str(product), product.name)
-        category = Category(name="Category Name")
-        self.assertEqual(str(category), category.name)
-        subcategory = SubCategory(name="Sub Name")
-        self.assertEqual(str(subcategory), subcategory.name)
+        category = Category(friendly_name="Category Name")
+        self.assertEqual(str(category), category.friendly_name)
+        subcategory = SubCategory(friendly_name="Sub Name")
+        self.assertEqual(str(subcategory), subcategory.friendly_name)
 
     def test_verbose_name_plural(self):
         self.assertEqual(str(Category._meta.verbose_name_plural), "Categories")
