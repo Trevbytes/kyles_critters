47d15ab9867c8799dc646549214c39e6cb138640
Trevbytes <trevor.j.adams@outlook.com>
2020-08-27 09:16:29 +0000

    Add next test

--- home/tests.py
+++ home/tests.py
@@ -19,3 +19,12 @@ class HomePageTests(TestCase):
         self.assertContains(response, '1-name')
         self.assertContains(response, '1-description')
         self.assertContains(response, '2-name')
+
+    def test_one_not_featured_and_one_featured_product(self):
+        Product.objects.create(name='1-name', description='1-description', price=3.7, featured=True)
+        Product.objects.create(name='2-name', description='2-description', price=1.0, featured=False)
+        response = self.client.get('')
+        self.assertContains(response, '1-name')
+        self.assertContains(response, '1-description')
+        self.assertContains(response, 3.7)
+        self.assertNotContains(response, '2-name')
