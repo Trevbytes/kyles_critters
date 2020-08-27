2b2f75fb8511978f70e7c59817d75ebcdeed3835
Trevbytes <trevor.j.adams@outlook.com>
2020-08-27 12:23:08 +0000

    Refactor tests, remove description from homepage

--- home/tests.py
+++ home/tests.py
@@ -10,14 +10,12 @@ class HomePageTests(TestCase):
         Product.objects.create(name='1-name', description='1-description', price=5.8, featured=True)
         response = self.client.get('')
         self.assertContains(response, '1-name')
-        self.assertContains(response, '1-description')
 
     def test_two_products(self):
         Product.objects.create(name='1-name', description='1-description', price=3.7, featured=True)
         Product.objects.create(name='2-name', description='2-description', price=1.0, featured=True)
         response = self.client.get('')
         self.assertContains(response, '1-name')
-        self.assertContains(response, '1-description')
         self.assertContains(response, '2-name')
 
     def test_one_not_featured_and_one_featured_product(self):
@@ -25,6 +23,5 @@ class HomePageTests(TestCase):
         Product.objects.create(name='2-name', description='2-description', price=1.0, featured=False)
         response = self.client.get('')
         self.assertContains(response, '1-name')
-        self.assertContains(response, '1-description')
         self.assertContains(response, 3.7)
         self.assertNotContains(response, '2-name')
