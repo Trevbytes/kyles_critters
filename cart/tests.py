6fde4090af1dd0c6c6b826aaa0be2fae8972c4cf
Trevbytes <trevor.j.adams@outlook.com>
2020-09-01 11:46:57 +0000

    add cart form test

--- cart/tests.py
+++ cart/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
-
+from .forms import CartForm
+from products.models import Product
 # Create your tests here.
+
+
+class AddToCartFormTest(TestCase):
+
+    def setUp(self):
+        Product.objects.create(name='1-name',
+                               description='1-description',
+                               price=5.8)
+
+    def test_init(self):
+        CartForm(entry=self.name)
