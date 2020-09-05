00e98e58ffac641aa1ed8b01f604390b28b86e46
Trevbytes <trevor.j.adams@outlook.com>
2020-09-05 16:13:54 +0000

    Refactor and style login and register page

--- kyles_critters/settings.py
+++ kyles_critters/settings.py
@@ -109,7 +109,7 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
-SITE_ID = 1
+SITE_ID = 2
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
