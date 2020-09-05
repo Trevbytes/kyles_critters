1235284764bfd125a25a1477e9472a54deea12cb
Trevbytes <trevor.j.adams@outlook.com>
2020-09-05 21:57:12 +0000

    Add next test

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 class ProfileModelTest(TestCase):
@@ -19,3 +20,15 @@ class ProfileModelTest(TestCase):
     def test_string_representation_profile(self):
         profile = UserProfile.objects.get(default_phone_number="252353")
         self.assertEqual(str(profile), profile.user.username)
+
+
+class ProfileFormTest(TestCase):
+
+    def test_fields_are_explicit_in_user_form_metaclass(self):
+        form = UserProfileForm()
+        self.assertEqual(form.Meta.fields, ('default_phone_number',
+                                            'default_street_address1',
+                                            'default_street_address2',
+                                            'default_town_or_city',
+                                            'default_postcode',
+                                            'default_county'))
