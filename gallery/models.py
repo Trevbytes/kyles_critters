91f8f8dfe4711db2bc8b36c0cde22a3520b2ac0a
Trevbytes <trevor.j.adams@outlook.com>
2020-09-08 10:56:26 +0000

    All tests pass

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,37 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from products.models import Product
+from profiles.models import UserProfile
+from cloudinary.models import CloudinaryField
+
+
+class GalleryEntry(models.Model):
+    entry_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='entries')
+    critter_name = models.CharField(max_length=50, null=False, blank=False)
+    critter_type = models.ForeignKey(Product, null=True, blank=True,
+                                 on_delete=models.SET_NULL)
+    critter_info = models.TextField(null=True, blank=True)
+    # Points to a Cloudinary image
+    image = CloudinaryField('image', null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def _generate_entry_number(self):
+        """
+        Generate a random, unique request number using UUID
+        """
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it hasn't been set already.
+        """
+        if not self.entry_number:
+            self.entry_number = self._generate_entry_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.entry_number
--- gallery/tests.py
+++ gallery/tests.py
@@ -7,9 +7,7 @@ class GalleryModelTest(TestCase):
     def setUp(self):
         GalleryEntry.objects.create(entry_number="123",
                                     critter_name="Test Name",
-                                    critter_type="Chicken",
                                     critter_info="Test info",
-                                    submitted_by="mouse",
                                     date="22/23/2000",)
 
     def test_string_representation_entry(self):
