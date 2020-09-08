027bfa7261c919867ae97a636f1b382e729887dd
Trevbytes <trevor.j.adams@outlook.com>
2020-09-08 10:36:06 +0000

    Add next test

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import GalleryEntry
 
-# Create your tests here.
+
+class GalleryModelTest(TestCase):
+
+    def setUp(self):
+        GalleryEntry.objects.create(entry_number="123",
+                                    critter_name="Test Name",
+                                    critter_type="Chicken",
+                                    critter_info="Test info",
+                                    submitted_by="mouse",
+                                    date="22/23/2000",)
+
+    def test_string_representation_entry(self):
+        gallery_entry = GalleryEntry.objects.get(entry_number="123")
+        self.assertEqual(str(gallery_entry), gallery_entry.entry_number)
