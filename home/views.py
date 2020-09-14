c148556b7934c0980b12a7dd016ba5d85b2c1b47
Trevbytes <trevor.j.adams@outlook.com>
2020-09-14 07:53:58 +0000

    Refactor product card stack to a single file

--- home/views.py
+++ home/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from products.models import Product
+from django.db.models import Q
 # Create your views here.
 
 
 def index(request):
     """ A view to return the index page """
 
-    products = Product.objects.all()
+    products = Product.objects.filter(Q(featured=True))
     context = {
         'products': products,
     }
