5a1e9fc8990a4ae92019539dfe1e510742d249cd
Trevbytes <trevor.j.adams@outlook.com>
2020-10-02 12:12:04 +0000

    Clean gallery entries in admin site

--- gallery/admin.py
+++ gallery/admin.py
@@ -3,4 +3,17 @@ from django.contrib import admin
 from .models import GalleryEntry
 
 
-admin.site.register(GalleryEntry)
+class GalleryAdmin(admin.ModelAdmin):
+
+    readonly_fields = ('entry_number', 'user_profile', 'date')
+
+    fields = ('entry_number', 'user_profile', 'date', 'critter_name',
+              'critter_type', 'critter_info', 'image')
+
+    list_display = ('date', 'user_profile',
+                    'critter_type', 'entry_number',)
+
+    ordering = ('-date',)
+
+
+admin.site.register(GalleryEntry, GalleryAdmin)
--- gallery/models.py
+++ gallery/models.py
@@ -8,6 +8,10 @@ from ckeditor.fields import RichTextField
 
 
 class GalleryEntry(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Gallery Entries"
+
     entry_number = models.CharField(max_length=32, null=False, editable=False)
     user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
                                      null=True, blank=True, related_name='entries')
