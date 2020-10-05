48113a6da6d6699ef116ba1ad112f5cf963fa5ed
Trevbytes <trevor.j.adams@outlook.com>
2020-10-05 11:01:15 +0000

    Remove auto focus

--- home/forms.py
+++ home/forms.py
@@ -4,9 +4,9 @@ from ckeditor.widgets import CKEditorWidget
 
 class MessageForm(forms.Form):
 
-    full_name = forms.CharField(max_length=50)
-    email = forms.EmailField(max_length=254)
-    phone_number = forms.CharField(max_length=20)
+    message_full_name = forms.CharField(max_length=50)
+    message_email = forms.EmailField(max_length=254)
+    message_phone_number = forms.CharField(max_length=20)
     message_subject = forms.CharField(max_length=60)
     message_body = forms.CharField(widget=CKEditorWidget())
 
@@ -17,13 +17,12 @@ class MessageForm(forms.Form):
         """
         super().__init__(*args, **kwargs)
         placeholders = {
-            'full_name': 'Full Name',
-            'email': 'Email Address',
-            'phone_number': 'Phone Number',
+            'message_full_name': 'Full Name',
+            'message_email': 'Email Address',
+            'message_phone_number': 'Phone Number',
             'message_subject': 'Message Subject',
         }
 
-        self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
             if field != 'message_body':
                 placeholder = f'{placeholders[field]} *'
