3e1e80fa8edf140e89cd0b8cb0b12836728f66a5
Trevbytes <trevor.j.adams@outlook.com>
2020-10-06 16:07:54 +0000

    Remove duplicate country placeholder

--- checkout/forms.py
+++ checkout/forms.py
@@ -18,24 +18,23 @@ class OrderForm(forms.ModelForm):
         """
         super().__init__(*args, **kwargs)
         placeholders = {
-            'full_name': 'Full Name',
-            'email': 'Email Address',
-            'phone_number': 'Phone Number',
-            'country': 'Country',
+            'full_name': 'Full Name*',
+            'email': 'Email Address*',
+            'phone_number': 'Phone Number*',
             'postcode': 'Postal Code',
-            'town_or_city': 'Town or City',
-            'street_address1': 'Street Address 1',
+            'town_or_city': 'Town or City*',
+            'street_address1': 'Street Address 1*',
             'street_address2': 'Street Address 2',
             'county': 'County',
         }
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
+            if field != 'country':
                 placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            else:
+                self.fields['country'].widget.attrs['class'] = 'browser-default \
+                    custom-select'
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
-            self.fields['country'].widget.attrs['class'] = 'browser-default custom-select'
