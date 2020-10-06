b5b8b3f88139ead111618f837f6d4ce5c1c01ef2
Trevbytes <trevor.j.adams@outlook.com>
2020-10-06 18:38:49 +0000

    Remove unused code

--- loan/models.py
+++ loan/models.py
@@ -2,7 +2,6 @@ import uuid
 
 from django.db import models
 from django_countries.fields import CountryField
-from products.models import Product
 from profiles.models import UserProfile
 from ckeditor.fields import RichTextField
 
